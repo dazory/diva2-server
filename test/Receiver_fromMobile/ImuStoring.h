ee9b2121ed6d279575cad1ad9a14a6b9060cca8c
airpod2 <thvldk40@naver.com>
2021-04-13 12:24:08 +0900

    2021-04-13 dahye ImuStoring : add Storing to DB IMU Data(raw), service/MakeJson.h & service/MakeJson.cpp

--- test/Receiver_fromMobile/ImuStoring.h
+++ test/Receiver_fromMobile/ImuStoring.h
@@ -14,11 +14,13 @@
 #include <sys/time.h>
 #include <unistd.h>
 #include <fstream>
+#include <pqxx/pqxx>
 
 #include "mscl/mscl.h"
 #include "../service/ImuPacket.h"
 
 using namespace std;
+using namespace pqxx;
 
 class ImuStoringThread
 {
@@ -29,9 +31,6 @@ public:
 
 private:
     bool stop_flag = false;
-    // float accel_x = 0.1;
-    // float accel_y = 0.1;
-    // float accel_z = 0.1;
 };
 
 ImuStoringThread::ImuStoringThread() {}
@@ -40,27 +39,79 @@ void ImuStoringThread::run(void *context)
 {
     printf("imu.run (in ImuParsingThread::run)\n");
     zmq::socket_t imu_rep(*(zmq::context_t *)context, ZMQ_REP);
-    imu_rep.bind("tcp://*:5564"); //Hmm....... // 172.31.47.43
+    imu_rep.bind("tcp://*:5564"); //165.246.39.124(cvlab2)
     printf("imu. Listening (in ImuStoringThread::run)\n");
+    connection C("dbname = ubuntu user = ubuntu password = diva2 \
+      hostaddr = 127.0.0.1 port = 5432"); //hostaddr and port?
+        if (C.is_open())
+            cout << "Connected to " << C.dbname() << endl;
+        work W(C); //text : variable unlimited length(=VARCAHR without (n))
+        W.exec("create table IMU_DATA(timestamp text, gyro_x text, gyro_y text,gyro_z text, acc_x text, acc_y text,acc_z text,\
+         mag_x text, mag_y text,mag_z text, roll text, pitch text, yaw text, roll_uncert text, pitch_uncert text, yaw_uncert text);"); // *****Set table and column*****
+        
+        //timestamp | gyrox | gyroy | gyroz | accelx | accely | accelz |  magx | magy | magz | roll | pitch | yaw | rolluncert | pitchuncert | yawuncert
 
     ImuPacket mImuPacket;
     while (!stop_flag)
     {
         cout << "while" << endl;
         zmq::message_t ImuData;
-        int rc = imu_rep.recv(&ImuData, 0);
+
+        int rc = false;
+        rc = imu_rep.recv(&ImuData, 0);
+
         if (rc)
         {
             memcpy(&mImuPacket, ImuData.data(), ImuData.size());
             cout<<"ImuData packet size :" <<ImuData.size()<<endl;
-            cout << "memory copy clear!" << endl;
 
-            printf("Accel (X,Y,Z) = (%.2f, %.2f, %.2f)\n", mImuPacket.scaledAccelX, mImuPacket.scaledAccelY, mImuPacket.scaledAccelZ);
-            printf("Gyro (X,Y,Z) = (%.2f, %.2f, %.2f)\n", mImuPacket.scaledGyroX, mImuPacket.scaledGyroY, mImuPacket.scaledGyroZ);
-            printf("Mag (X,Y,Z) = (%.2f, %.2f, %.2f)\n", mImuPacket.scaledMagX, mImuPacket.scaledMagY, mImuPacket.scaledMagZ);
-            printf("est (Roll,Pitch,Yaw) = (%.2f, %.2f, %.2f)\n", mImuPacket.estRoll, mImuPacket.estPitch, mImuPacket.estYaw);
-            printf("estUncert (Roll,Pitch,Yaw) = (%.2f, %.2f, %.2f)\n", mImuPacket.estRollUncert, mImuPacket.estPitchUncert, mImuPacket.estYawUncert);
-            cout << "recv" << endl;
+            // Execute SQL query : Storing IMU Data(raw) to DB
+            string query_string = "insert IMU_DATA(";
+            query_string.append(to_string(mImuPacket.time));
+            query_string.append("', ");
+            query_string.append(to_string(mImuPacket.scaledGyroX));
+            query_string.append(", ");
+            query_string.append(to_string(mImuPacket.scaledGyroY));
+            query_string.append(", ");
+            query_string.append(to_string(mImuPacket.scaledGyroZ));
+            query_string.append(", ");
+            query_string.append(to_string(mImuPacket.scaledAccelX));
+            query_string.append(", ");
+            query_string.append(to_string(mImuPacket.scaledAccelY));
+            query_string.append(", ");
+            query_string.append(to_string(mImuPacket.scaledAccelZ));
+            query_string.append(", ");
+            query_string.append(to_string(mImuPacket.scaledMagX));
+            query_string.append(", ");
+            query_string.append(to_string(mImuPacket.scaledMagY));
+            query_string.append(", ");
+            query_string.append(to_string(mImuPacket.scaledMagZ));
+            query_string.append(", ");
+            query_string.append(to_string(mImuPacket.estRoll));
+            query_string.append(", ");
+            query_string.append(to_string(mImuPacket.estYaw));
+            query_string.append(", ");
+            query_string.append(to_string(mImuPacket.estPitch));
+            query_string.append(", ");
+            query_string.append(to_string(mImuPacket.estRollUncert));
+            query_string.append(", ");
+            query_string.append(to_string(mImuPacket.estPitchUncert));
+            query_string.append(", ");
+            query_string.append(to_string(mImuPacket.estYawUncert));
+            query_string.append(");");
+            W.exec(query_string);
+            W.commit();
+            std::cout << "IMU_DATA storing to DB" <<endl;
+
+            //~~~~~~~~~ check the packet ~~~~~~~~~~
+            // printf("Gyro (X,Y,Z) = (%.2f, %.2f, %.2f)\n", mImuPacket.scaledGyroX, mImuPacket.scaledGyroY, mImuPacket.scaledGyroZ);
+            // printf("Accel (X,Y,Z) = (%.2f, %.2f, %.2f)\n", mImuPacket.scaledAccelX, mImuPacket.scaledAccelY, mImuPacket.scaledAccelZ);
+            // printf("Mag (X,Y,Z) = (%.2f, %.2f, %.2f)\n", mImuPacket.scaledMagX, mImuPacket.scaledMagY, mImuPacket.scaledMagZ);
+            // printf("est (Roll,Pitch,Yaw) = (%.2f, %.2f, %.2f)\n", mImuPacket.estRoll, mImuPacket.estPitch, mImuPacket.estYaw);
+            // printf("estUncert (Roll,Pitch,Yaw) = (%.2f, %.2f, %.2f)\n", mImuPacket.estRollUncert, mImuPacket.estPitchUncert, mImuPacket.estYawUncert);
+            //cout << "recv" << endl; 
+
+            //~~~ send OK msg to Mobile ~~~
             string strOK = "thank you";
             zmq::message_t zmqData(sizeof(strOK));
             memcpy(zmqData.data(), strOK.c_str(), sizeof(strOK));
@@ -68,16 +119,28 @@ void ImuStoringThread::run(void *context)
             if (ok)
                 cout << "send ok" << endl;
         }
-        else
+        else //receive failed -> send not ok msg to Mobile 
         {
-            string strOK = "recive fail";
-            zmq::message_t zmqData(sizeof(strOK));
-            memcpy(zmqData.data(), strOK.c_str(), sizeof(strOK));
-            int ok = imu_rep.send(zmqData, 0);
-            if (ok)
-                cout << "send ok" << endl;
+            string strNotOk = "receive failed";
+            zmq::message_t zmqData(sizeof(strNotOk));
+            memcpy(zmqData.data(), strNotOk.c_str(), sizeof(strNotOk));
+            int NotOk = imu_rep.send(zmqData, 0);
+            if (NotOk)
+                cout << "waiting...." << endl;
         }
-        //string str;
+        
+    }
+    imu_rep.close();
+}
+
+void ImuStoringThread::stop()
+{
+    stop_flag = true;
+}
+
+
+
+//string str;
         //memcpy(&str,strdata.data(),strdata.size());
         //printf("ANS: %d bytes \n", ImuData.size());
 
@@ -87,11 +150,3 @@ void ImuStoringThread::run(void *context)
                 int ok = imu_rep.send(zmqData,0);
                 if(ok)
                         cout<<"send ok"<<endl;*/
-    }
-    imu_rep.close();
-}
-
-void ImuStoringThread::stop()
-{
-    stop_flag = true;
-}
