476dd5e24583f460a201e00ec289b19b7bd2600e
kayeon <oooac999@gmail.com>
2021-04-21 15:04:20 +0900

    receiver from mobile - 'gps' receiver, json storing

--- test/Receiver_fromMobile/main.cpp
+++ test/Receiver_fromMobile/main.cpp
@@ -1,14 +1,23 @@
 #pragma once
-#include "ImuStoring.h"
+#include "../service/global_name.hpp"
+#include "GpsReceiverThread.h"
 #include <zmq.hpp>
-#include <thread>
+#include <thread> 
+
+using namespace std;
 
 int main(int argc, char *argv[]){
-        // REP
-        zmq::context_t contextRep(1);
-        ImuStoringThread mImuReceiverThread;
-        thread imuSenderThread(&ImuStoringThread::run, &mImuReceiverThread, &contextRep);
-        imuSenderThread.join();
+    
+    // REP
+    zmq::context_t contextRep(1);
+    // zmq::socket_t socketRep(contextRep, ZMQ_REP);
+    // socketRep.connect(protocol::RECEIVER_FROMMOBILE_REP_TEST);
+    // printf("rep socket connected (in main)\n");
 
-        return 0;
+    // GPS
+    GpsReceiverThread mGpsReceiverThread;
+    thread gpsSenderThread(&GpsReceiverThread::run, &mGpsReceiverThread, &contextRep);
+    gpsSenderThread.join();
+
+
 }
